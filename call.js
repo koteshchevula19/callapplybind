615ff1267bf537587ac9604b295652926be1bd57
koteshchevula19 <koteshchevula11@gmail.com>
2023-08-24 15:39:51 +0530

    first commit

---
+++ call.js
@@ -0,0 +1,10 @@
+
+var person1 = {firstName: 'Jon', lastName: 'Kuperman'};
+var person2 = {firstName: 'Kelly', lastName: 'King'};
+
+function say(greeting) {
+    console.log(greeting + ' ' + this.firstName + ' ' + this.lastName);
+}
+
+say.call(person1, 'Hello'); // Hello Jon Kuperman
+say.call(person2, 'Hello'); // Hello Kelly King
